d4071b052ac3352ffdf95964e658c64567ec90d6
Maria Antonieta Pereira <antonietap0@gmail.com>
2022-09-09 15:05:20 -0500

    exercise task ok

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,25 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # Mostrar todas tareas
+  get '/tasks', to: 'tasks#index'
+
+  get 'tasks/new', to: 'tasks#new'
+
+  # Mostrar una tarea
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # Agregar una tarea
+
+  post 'tasks', to: 'tasks#create'
+
+  # Editar una tarea
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Eliminar tarea
+
+  delete 'tasks/:id', to: 'tasks#destroy'
+
 end
